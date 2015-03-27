30acf85e65b786e98cada780f519dc69f313a89d
msrandhawa <msrandhawa31@gmail.com>
2015-03-27 12:34:25 -0700

    Update APITests.groovy
    Can you kindly add a method to test the JSON response received from a remote web service that consumes JSON data in request? 
    I have added a snippet in there but it's not working properly.

--- test/functional/APITests.groovy
+++ test/functional/APITests.groovy
@@ -14,4 +14,22 @@ class APITests extends APITestCase {
 
         assertStatus 200
     }
+    
+    /**
+     * Test the JSON response received from a remote web service that consumes JSON data in request
+    */
+    void testPost() {
+        post("http://httpbin.org/post", {
+            param1 = "value1"
+            param2 = "value2"
+        })
+        
+        assertStatus 200
+        
+        def model = this.response.contentAsString
+        def map = JSON.parse(model)
+        
+        assertNotNull(map.attributes.id)
+    }
+    
 }
